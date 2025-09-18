b21db07cbed1fa7f0258aad250d785839b2f3cc0
Aman kumar <shreya.sharma7110@gmail.com>
2025-09-18 11:03:20 +0530

    db update1

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,28 @@
+// import mongoose from "mongoose";
+
+// Function to connect to the MongoDB database
+// const connectDB = async () => {
+//   mongoose.connection.on('connected', () => console.log('✅ Database Connected'));
+
+//   // Do NOT append /job-portal again because URI already has it
+//   await mongoose.connect(process.env.MONGODB_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   });
+// };
+
+// export default connectDB;
+
+
 import mongoose from "mongoose";
 
 // Function to connect to the MongoDB database
 const connectDB = async () => {
-  mongoose.connection.on('connected', () => console.log('✅ Database Connected'));
 
-  // Do NOT append /job-portal again because URI already has it
-  await mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-};
+    mongoose.connection.on('connected', () => console.log('Database Connected'))
 
-export default connectDB;
+    await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`)
+
+}
+
+export default connectDB
